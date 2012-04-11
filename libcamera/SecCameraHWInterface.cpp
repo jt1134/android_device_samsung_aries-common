71a378236e1f6e4545474ffdce5059ee2e64a472
jt1134 <jt1134@gmail.com>
2012-04-11 01:19:03 -0500

    libcamera: fix preview and snapshot sizes for M5MO camera
    allows photos up to 8MP
    
    Change-Id: I823d2adf097231855ab6a237568ff638c8e569bb

--- libcamera/SecCameraHWInterface.cpp
+++ libcamera/SecCameraHWInterface.cpp
@@ -156,10 +156,17 @@ void CameraHardwareSec::initDefaultParameters(int cameraId)
     int snapshot_max_height = 0;
 
     if (cameraId == SecCamera::CAMERA_ID_BACK) {
+#ifdef M5MO_CAMERA
+        p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
+              "1920x1080,1280x720,800x480,720x480,640x480,528x432,320x240,176x144");
+        p.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
+              "3264x2448,3264x1968,2560x1920,2560x1536,2048x1536,2048x1232,1600x1200,1600x960,800x480,640x480");
+#else
         p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
               "1280x720,800x480,720x480,640x480,592x480,352x288");
         p.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
               "2560x1920,2560x1536,2048x1536,2048x1232,1600x1200,1600x960,800x480,640x480");
+#endif
     } else {
         p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
               "640x480,320x240,176x144");
